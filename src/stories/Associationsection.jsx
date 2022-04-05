3c95ad3be95087f5cc31d77a0abadc2c913ebbb1
orhanelahneche <orhane.lahneche@etu.utc.fr>
2022-04-05 09:01:59 +0200

    autre

---
+++ src/stories/Associationsection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Eventsection.css'
+
+function Associationsection () {
+  return(
+  <div className="event-section" id="Associationsection">
+    <div className="event-section-title">
+      <hr></hr>
+      <h1>L'association</h1>
+      <hr></hr>
+    </div>
+  </div>
+)
+}
+
+export default Associationsection
